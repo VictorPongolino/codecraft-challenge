b7eea2f341ac7fcfcbb90c3218005900d7288e89
victor.pongolino <victor.pongolino@hotmail.com>
2023-12-01 23:23:11 -0300

    Moving project to correct path, FIX.

--- movies-service/src/main/java/com/pongolino/study/reactive/domain/repository/MovieInfoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pongolino.study.reactive.domain.repository;
-
-import com.pongolino.study.reactive.domain.entity.Movie;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-public interface MovieInfoRepository extends ReactiveMongoRepository<Movie, String> {
-}
