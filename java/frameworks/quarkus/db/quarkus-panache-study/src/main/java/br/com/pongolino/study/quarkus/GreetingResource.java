4680107affa78ea52dff656b666a5ec74705f243
victor.pongolino <victor.pongolino@hotmail.com>
2023-12-22 19:37:00 -0300

    BASE project related to practise Panache+JPA.

---
+++ java/frameworks/quarkus/db/quarkus-panache-study/src/main/java/br/com/pongolino/study/quarkus/GreetingResource.java
@@ -0,0 +1,16 @@
+package br.com.pongolino.study.quarkus;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/C:/Program Files/Git/hello")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello RESTEasy";
+    }
+}
